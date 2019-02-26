50e6e3d013318be6d5779f2eeccf4271d89b4554
Anmol Mehta <46704633+meta-anmol-mehta@users.noreply.github.com>
2019-02-26 10:48:23 +0530

    Update MyDatabaseQueries.java

--- src/com/metacube/training/database/MyDatabaseQueries.java
+++ src/com/metacube/training/database/MyDatabaseQueries.java
@@ -16,9 +16,9 @@ public class MyDatabaseQueries {
 	public static final String FIND_CATEGORY = "SELECT * FROM category where categoryID=?";
 	public static final String INSERT_ADVERTISEMENT = "INSERT INTO advertisement (advertisementID,advertisementTitle, advertisementDesc , categoryID) VALUES (?,?,?,?)";
 	public static final String INSERT_CATEGORY = "INSERT INTO category (categoryID,categoryName) VALUES (?,?)";
-	public static final String UPDATE_CATEGORY = "UPDATE category SET categoryName = ? WHERE id = ? ";
-	public static final String UPDATE_ADVERTISEMENT = "UPDATE advertisement SET advertisementTitle = ?,advertisementDesc=? WHERE id = ? ";
-	public static final String DELETE_ADVERTISEMENT = "DELETE FROM advertisement WHERE id = ? ";
-	public static final String DELETE_CATEGORY = "DELETE FROM category WHERE id = ? ";
+	public static final String UPDATE_CATEGORY = "UPDATE category SET categoryName = ? WHERE categoryID = ? ";
+	public static final String UPDATE_ADVERTISEMENT = "UPDATE advertisement SET advertisementTitle = ?,advertisementDesc=? WHERE advertisementID = ? ";
+	public static final String DELETE_ADVERTISEMENT = "DELETE FROM advertisement WHERE advertisementID = ? ";
+	public static final String DELETE_CATEGORY = "DELETE FROM category WHERE categoryID = ? ";
 
 }
