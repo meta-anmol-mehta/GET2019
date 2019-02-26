b2f44be8fb23c101b7fc025836022f6adaa7ce6c
Anmol Mehta <46704633+meta-anmol-mehta@users.noreply.github.com>
2019-02-26 12:01:54 +0530

    Update TriangleArea.java

--- EAD-Assignment-SOAP/src/com/metacube/training/soap/TriangleArea.java
+++ EAD-Assignment-SOAP/src/com/metacube/training/soap/TriangleArea.java
@@ -2,9 +2,18 @@ package com.metacube.training.soap;
 
 public class TriangleArea{
 
-	public double area(double side1,double side2,double side3){
-		double perimeter=(side1+side2+side3)/2;
-		double result=Math.sqrt((perimeter)*(perimeter-side1)*(perimeter-side2)*(perimeter-side3));
-		return result;
+	public String area(String side1,String side2,String side3){
+		
+		if((side1 != null && side1.matches("[0-9]+"))||(side2 != null && side2.matches("[0-9]+"))||(side3 != null && side3.matches("[0-9]+"))){
+			return "enter valid positive side.";
+		}
+		else{
+			Double parseSide1=Double.parseDouble(side1);
+			Double parseSide2=Double.parseDouble(side2);
+			Double parseSide3=Double.parseDouble(side3);
+			double perimeter=(parseSide1+parseSide2+parseSide3)/2;
+			double result=Math.sqrt((perimeter)*(perimeter-parseSide1)*(perimeter-parseSide2)*(perimeter-parseSide3));
+			return String.valueOf(result);
+		}
 	}
 }
